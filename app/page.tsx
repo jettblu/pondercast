6f2fb9f492cc89de8231d6eefcdc9a906d860348
jettblu <jettblu.fabrications@gmail.com>
2023-07-28 11:14:06 -0700

    Add episode player

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import Image from 'next/image'
+import { Spotify } from 'react-spotify-embed'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-4">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Ponder the mysteries of life with us.
@@ -51,7 +52,8 @@ export default function Home() {
              <p className='text-center dark:text-gray-200 text-gray-700 font-semibold bg-gray-500/20 dark:bg-gray-400/20 p-2 rounded-xl'>A podcast full of perplexing questions and provocative opinions.</p>
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className="mt-4 mb-32 grid text-center lg:mb-0 lg:grid-cols-1 lg:text-left">
+        <Spotify wide link="https://open.spotify.com/show/1KEm0JWmjoZIO4fqNHQ51E" />
         {/* <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
